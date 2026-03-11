d424c16d167bda4e27c9771eb9f51d2e85e49c68
RamCodeZ3 <arammusset7@gmail.com>
2026-03-10 20:37:29 -0400

    refactor: smaller chages

--- app/db/database.py
+++ app/db/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
 
+
 DATABASE_URL = "sqlite:///itla_bot.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # SQLite ignora foreign keys por defecto — esto las activa
 @event.listens_for(engine, "connect")
