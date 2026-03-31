26e1c397f31433e778b9ad6d232e805307df4242
RamCodeZ3 <arammusset7@gmail.com>
2026-03-30 23:43:31 -0400

    feature: se agrego el comando /help al bot para que muestre los comandos disponibles en el bot.

---
+++ app/bot/cogs/help_cog.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @app_commands.command(
+        name="help",
+        description="Comando para mostrar todos los comandos disponible"
+        " que tiene el bot"
+    )
+    async def help_command(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="📜 Lista de comandos",
+            description="Comandos que le permitira interactuar con el bot.",
+            color=0xFFFFFF
+        )
+
+        embed.add_field(
+            name= "**/register**",
+            value=(
+                "El comando **/register** se utiliza para registrate con tus \n"
+                "crendeciales (correo y contraseña) del campus virtual."
+            )
+        )
+
+        embed.add_field(
+            name="**/set-schedule**",
+            value=(
+                "Este comando se utiliza para configurar tu horario de rutas del ITLA.\n"
+                "**1.** Primero te mostrará un modal donde deberás ingresar el cuatrimestre "
+                "en el que te encuentras (ej: `2026-1`).\n\n"
+                "**2.** Luego podrás seleccionar los **días** en los que asistirás al ITLA "
+                "(de lunes a sábado).\n\n"
+                "**3.** Por cada día seleccionado, configurarás mediante **menús desplegables**:\n"
+                "\u20031. 🕐 Hora de llegada al ITLA\n"
+                "\u20032. 🚌 Parada de recogida\n"
+                "\u20033. 🕓 Hora de salida del ITLA\n"
+                "\u20034. 🏁 Parada final de la ruta"
+            ),
+            inline=False
+        )
+
+        await interaction.response.send_message(embed=embed)
+    
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
--- app/scraper/scrapper_ticket.py
+++ app/scraper/scrapper_ticket.py
@@ -26,7 +26,7 @@ class ITLAScraper:
 
     async def run(self):
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch()
             context = await browser.new_context()
             page = await context.new_page()
             ticket_downloader = TicketDownloader()
