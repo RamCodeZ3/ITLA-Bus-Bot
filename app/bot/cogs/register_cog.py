11956ba6951d0d5ee47f78b95c0b2657417e9909
RamCodeZ3 <arammusset7@gmail.com>
2026-04-19 16:11:29 -0400

    feat: se añadio nuevos apartados para la compra de los boletos

--- app/bot/cogs/register_cog.py
+++ app/bot/cogs/register_cog.py
@@ -8,37 +8,36 @@ from infrastructure.database import get_session
 class Register(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @app_commands.command(
         name="register",
-        description="Comando para registrar el usuario con"
-        "sus credenciales del campus ITLA"
+        description="Comando para registrar el usuario con "
+        "sus credenciales del campus ITLA",
     )
     async def register_command(
         self,
         interaction: discord.Interaction,
         email: str,
-        password: str
+        password: str,
     ):
+        await interaction.response.defer(ephemeral=True)
+
+        session = get_session()
         try:
-            session = get_session()
-            
             repository = UserRepository(session)
             repository.create(
                 discord_id=interaction.user.id,
                 email=email,
-                password=password  # directo sin encriptar
+                password=password, # Directo sin encriptar
             )
-
-            await interaction.response.send_message(
-                "Te registraste de manera exitosa", 
-                ephemeral=True
+            await interaction.followup.send(
+                "✅ Te registraste de manera exitosa.", ephemeral=True
             )
         except Exception as e:
-            await interaction.response.send_message(
-                "Ocurrió un error al registrarte",
-                ephemeral=True
+            await interaction.followup.send(
+                "❌ Ocurrió un error al registrarte.", ephemeral=True
             )
+            print(f"[Register] Error: {e}")
         finally:
             session.close()
 
